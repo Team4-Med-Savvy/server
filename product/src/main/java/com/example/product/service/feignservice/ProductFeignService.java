f65af7d00929ec312a906ba139a15416423edcc4
vinaymatta <vinaymatta63@gmail.com>
2022-01-26 12:17:18 +0530

    feign

--- product/src/main/java/com/example/product/service/feignservice/ProductFeignService.java
+++ product/src/main/java/com/example/product/service/feignservice/ProductFeignService.java
@@ -1,4 +1,12 @@
 package com.example.product.service.feignservice;
 
+import com.example.product.dto.MerchantDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="product")
 public interface ProductFeignService {
+    @GetMapping("/merchant/{id}")
+    MerchantDto findById(@PathVariable Long id);
 }
