e9fe51e83492d26260a8d6694289da1ba99b4ba1
vinaymatta <vinaymatta63@gmail.com>
2022-01-26 11:35:25 +0530

    check

--- product/src/main/java/com/example/product/controller/CategoryController.java
+++ product/src/main/java/com/example/product/controller/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Method;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
