57d66fd43d11a4d33e948491c6fc77b42fbc9043
Sourav Borah <souravborah@Souravs-MacBook-Pro.local>
2022-01-28 00:29:35 +0530

    push

--- cart-service/src/main/java/com/example/cartservice/controller/CartController.java
+++ cart-service/src/main/java/com/example/cartservice/controller/CartController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @RestController
 @RequestMapping(value="/cart")
 public class CartController {
