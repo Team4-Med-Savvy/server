2df7c133ec40a21d0df89205841a2b5081394460
vinaymatta <vinaymatta63@gmail.com>
2022-01-28 12:25:17 +0530

    category route

--- product/src/main/java/com/example/product/service/CategoryService.java
+++ product/src/main/java/com/example/product/service/CategoryService.java
@@ -2,9 +2,12 @@ package com.example.product.service;
 
 import com.example.product.entity.Category;
 
+import java.util.List;
+
 public interface CategoryService {
     Category findByName(String id);
     void save(Category category);
     void  delete(String id);
+    List<Category> findAll();
 
 }
